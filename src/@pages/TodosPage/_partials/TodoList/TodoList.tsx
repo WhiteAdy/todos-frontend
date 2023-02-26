3de53b992d5e2d0abf335f92d78898b890ad6105
Mozartino <adi.tamimi@live.com>
2023-02-26 21:07:10 +0200

    implemented item filters

--- src/@pages/TodosPage/_partials/TodoList/TodoList.tsx
+++ src/@pages/TodosPage/_partials/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import TodoItem from '../TodoItem/TodoItem';
 import { todoDelete, todoPut } from './TodoList.api';
 import styles from './TodoList.module.scss';
@@ -9,6 +10,10 @@ function TodoList({ items }: ITodoList) {
 
 	const { mutate: patchTodoMutation } = useMutation(todoPut);
 	const { mutate: deleteTodoMutation } = useMutation(todoDelete);
+
+	const [activeFilter, setActiveFilter] = useState<
+		undefined | 'completed' | 'incompleted'
+	>();
 
 	const toggleTodoCompleted = (id: number, completed: boolean) => () => {
 		patchTodoMutation({ id, completed: !completed });
@@ -30,19 +35,61 @@ function TodoList({ items }: ITodoList) {
 		);
 	};
 
+	const filteredItems = items.filter((item) => {
+		switch (activeFilter) {
+			case 'completed':
+				return item.completed;
+			case 'incompleted':
+				return !item.completed;
+			default:
+				return item;
+		}
+	});
+
 	return (
-		<ul className={styles.TodoList}>
-			{items.map(({ id, title, completed }) => (
-				<TodoItem
-					key={`todo-${id}`}
-					id={id}
-					label={title}
-					onDelete={deleteTodo(id)}
-					onChange={toggleTodoCompleted(id, completed)}
-					checked={completed}
-				/>
-			))}
-		</ul>
+		<>
+			<ul className={styles.TodoList}>
+				{filteredItems.map(({ id, title, completed }) => (
+					<TodoItem
+						key={`todo-${id}`}
+						id={id}
+						label={title}
+						onDelete={deleteTodo(id)}
+						onChange={toggleTodoCompleted(id, completed)}
+						checked={completed}
+					/>
+				))}
+			</ul>
+			{items.length > 0 && (
+				<div className={styles.filter_container}>
+					<span className={styles.show_label}>Show:</span>
+					<label>
+						<input
+							type="radio"
+							name="show"
+							onChange={() => setActiveFilter(undefined)}
+						/>
+						<span>All</span>
+					</label>
+					<label>
+						<input
+							type="radio"
+							name="show"
+							onChange={() => setActiveFilter('completed')}
+						/>
+						<span>Completed</span>
+					</label>
+					<label>
+						<input
+							type="radio"
+							name="show"
+							onChange={() => setActiveFilter('incompleted')}
+						/>
+						<span>Incompleted</span>
+					</label>
+				</div>
+			)}
+		</>
 	);
 }
 export default TodoList;
